a689f37c1e861d74d5fa7e1f28c0b09a4a10e6b0
Jason L Perry <jasper@ambethia.com>
2015-10-05 11:35:47 -0400

    Remove routers.js; Add click event to index view

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,12 +23,23 @@ $(() => {
   var IndexView = Backbone.View.extend({
     template: $('#indexTemplate').text(),
 
+    events: {
+      // 'event selector': 'nameOfcallback'
+      // similar to: $('selector').on('event', nameOfCallback);
+
+      'click form.newGame button': 'createGame'
+    },
+
+    createGame: function (event) {
+      var difficulty = event.target.value;
+      console.log(difficulty);
+    },
+
     render: function() {
       this.$el.html(this.template);
       return this.el;   // 'this' is the JS IndexView instantiation
                         // 'el' is the html, or DOM, representation
-    },
-
+    }
   });
 
       //var indexTemplate = this.template(this.model.toJSON());
